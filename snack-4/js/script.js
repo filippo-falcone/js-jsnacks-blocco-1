929bf7481ec574f4f465ea27e94c420c96ab5f25
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-11 14:34:07 +0100

    feat: add array & for cycle

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -1,3 +1,20 @@
 /* ---------------------------------------------------
    Calcola la somma e la media dei primi 10 numeri.
 --------------------------------------------------- */
+/*
+// Creo un array
+// Creo un ciclo for
+// - Calcolo la somma
+// - Calcolo la media
+// Stampo entrambe le
+*/
+// Variabili
+const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let numberSum = 0;
+// Funzionalità
+for (let i = 0; i < 10; i++) {
+    console.log(numberList[i]);
+    numberSum += numberList[i];
+}
+// Output
+console.log(numberSum);
