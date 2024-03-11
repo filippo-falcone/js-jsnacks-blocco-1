29b241d93d3c73ed402385b95e156730ea2610d0
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-11 13:22:20 +0100

    feat: add array, for cicle & userNumber

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,3 +1,18 @@
 /* --------------------------------------------------------------------------------------------------------------------------------------------------
    Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare duplicati (inserire il numero nell'array solo se non è già presente)
 -------------------------------------------------------------------------------------------------------------------------------------------------- */
+/*
+// Creo un array
+// Creo un ciclo
+// - Chiedo all'utente di inserire 5 numeri
+// - Controllo se il numero non è gia presente
+// - Lo inserisco nell'array
+*/
+// Variabili
+const numberList = [];
+// Funzionlità
+for (let i = 0; i < 5; i++) {
+    const userNumber = parseInt(prompt('Inserisci un numero'));
+    console.log(userNumber);
+}
+// Output
