07e729f283d5ef8308d3b41b0f6ca883680e0d05
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-11 13:52:25 +0100

    feat: add for cycle

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -3,3 +3,17 @@
    - Chiedi per 6 volte all’utente di inserire un numero,
    - se è dispari inseriscilo nell’array.
 --------------------------------------------------------- */
+/*
+// Creo un array vuoto
+// Creo un ciclo for
+// - Chiedo all'utente di inserire un numero per 6 volte
+// - Verifico se è pario o dispari
+// -- Se è dispari lo aggiungo all'array
+*/
+// Varabili
+const numberList = [];
+// Funzionalità
+for (let i = 0; i < 6; i++) {
+    let userNumber = parseInt(prompt('Inserisci un numero'));
+    console.log(userNumber);
+}
