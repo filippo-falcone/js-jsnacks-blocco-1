14c8248910fb64252ebddd18f54e853814c95e70
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-11 13:42:19 +0100

    feat: add for cicle

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -2,3 +2,16 @@
    Il software deve chiedere per 5 volte all’utente di inserire un numero.
    - Il programma stampa la somma di tutti i numeri inseriti.
 -------------------------------------------------------------------------- */
+/*
+// Creo un ciclo for
+// - Chiedo all'utente di inserire un numero per 5 volte
+// - Sommo tutti i numeri
+// Stampo la somma
+*/
+// Variabili
+let numberSum = 0;
+// Funzinalità
+for (let i = 0; i < 5; i++) {
+    const userNumber = parseInt(prompt('Inserisci un numero'));
+    console.log(userNumber);
+}
