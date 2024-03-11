9cdb5a141610a5c7b3d424b241a9464c14ba7c19
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-11 15:44:58 +0100

    feat: add Math.max

--- snack-bonus/js/script.js
+++ snack-bonus/js/script.js
@@ -1,3 +1,14 @@
 /* --------------------------------------------------------------------------------
    Dato un array contenente 10 numeri, stampare in console il numero più grande.
 -------------------------------------------------------------------------------- */
+/*
+// Creo un array
+// Cerco il numero più grande
+// Stampo il numero più grande
+*/
+// Variabili
+const numberList = [15, 25, 48, 5, 88, 12, 1, 16, 100, 50];
+const numberLarge = Math.max(...numberList); // Applicazione trovata su mdn
+// Output
+alert('Il numero più grande è: ' + numberLarge);
+console.log(numberLarge);
